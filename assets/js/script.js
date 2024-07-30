f234aa60697afe77b97d4675b58908b6826ae1a0
TanishkKumar0409 <tanishkk60@gmail.com>
2024-07-30 19:31:29 +0530

    use prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,23 @@
-document
-.querySelectorAll(".offcanvas-menu nav ul li")
-.forEach((li, index) => {
+document.querySelectorAll(".offcanvas-menu nav ul li").forEach((li, index) => {
   li.style.setProperty("--i", index + 1);
 });
 
 window.addEventListener("scroll", function () {
-   var navigationbar = document.getElementById("navigationbar");
-   if (window.scrollY > 50) {
-     navigationbar.classList.add("scrolled");
-   } else {
-     navigationbar.classList.remove("scrolled");
-   }
- });
+  var navigationbar = document.getElementById("navigationbar");
+  if (window.scrollY > 50) {
+    navigationbar.classList.add("scrolled");
+  } else {
+    navigationbar.classList.remove("scrolled");
+  }
+});
 
- // Function to apply hover colors
+// Function to apply hover colors
 function applyHoverColors(selector, colors) {
-  document.querySelectorAll(selector)
-    .forEach((element, index) => {
-      // Cycle through colors using modulo
-      const colorIndex = index % colors.length;
-      element.style.setProperty('--color-hover', `var(${colors[colorIndex]})`);
-    });
+  document.querySelectorAll(selector).forEach((element, index) => {
+    // Cycle through colors using modulo
+    const colorIndex = index % colors.length;
+    element.style.setProperty("--color-hover", `var(${colors[colorIndex]})`);
+  });
 }
 
 // Colors for navigation items and social links
@@ -30,7 +27,7 @@ const hoverColors = [
   "--color-hover-3",
   "--color-hover-4",
   "--color-hover-5",
-  "--color-hover-6"
+  "--color-hover-6",
 ];
 
 applyHoverColors("#navigationbar li a", hoverColors);

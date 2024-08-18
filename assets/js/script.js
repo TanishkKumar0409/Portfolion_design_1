e9452deee0affe5b26dc94f706bb8c96f7726b97
TanishkKumar0409 <tanishkk60@gmail.com>
2024-08-18 09:21:47 +0530

    apearence animation

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+
+let Sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  let top = window.scrollY;
+
+  Sections.forEach((sec) => {
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+      sec.classList.add('show-animate');
+    }
+    else{
+      sec.classList.remove('show-animate');
+    }
+  });
+
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+  menuIcon.classList.remove('fa-xmark');
+  navbar.classList.remove('active');
+};
